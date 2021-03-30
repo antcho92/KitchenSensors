b4d993a81b76f12f9ed8679eb10a163dd21aad40
Anthony Cho <antcho92@gmail.com>
2021-03-29 21:28:45 -0700

    update console script

--- console_temp.py
+++ console_temp.py
@@ -1,3 +1,4 @@
+
 import adafruit_dht
 import paho.mqtt.client as paho
 import os
@@ -6,26 +7,36 @@ import RPi.GPIO as GPIO
 import board
 
 # --------- User Settings ---------
-SENSOR_LOCATION_NAME = "Bedroom"
+SENSOR_1 = "Sensor 1"
+SENSOR_2 = "Sensor 2"
 MINUTES_BETWEEN_READS = 1
 METRIC_UNITS = True
 # ---------------------------------
 
 dhtSensor = adafruit_dht.DHT22(board.D4)
+s2 = adafruit_dht.DHT22(board.D17)
 
 while True:
     try:
         humidity = dhtSensor.humidity
         temp_c = dhtSensor.temperature
+
+        h2 = s2.humidity
+        t2 = s2.temperature
+
+        if h2 is None or t2 is None:
+            print("h2 or t2 is null. Reconnect and try again.")
     except RuntimeError:
         print("RuntimeError, trying again...")
         continue
-            
-    if METRIC_UNITS:
-        print(f'{SENSOR_LOCATION_NAME} Temperature({temp_c})')
-    else:
-        temp_f = format(temp_c * 9.0 / 5.0 + 32.0, ".2f")
-        print(f'{SENSOR_LOCATION_NAME} Temperature({temp_f})')
-    humidity = format(humidity,".2f")
-    print(f'{SENSOR_LOCATION_NAME}  Humidity(%)({humidity})')
+
+    temp_f = format(temp_c * 9.0 / 5.0 + 32.0, ".2f")
+    t2_f = format(t2 * 9.0 / 5.0 + 32.0, ".2f")
+    print(f'{SENSOR_1}: Temperature: {temp_c} degrees celsius, {temp_f} degrees fahrenheit')
+    print(f'{SENSOR_2}: Temperature: {temp_c} degrees celsius, {temp_f} degrees fahrenheit')
+
+    humidity = format(humidity, ".2f")
+    humidity2 = format(h2, ".2f")
+    print(f'{SENSOR_1}: Humidity(%)({humidity})')
+    print(f'{SENSOR_2}:  Humidity(%)({h2})')
     time.sleep(60*MINUTES_BETWEEN_READS)
--- humidityTemperatureMQTT.py
+++ humidityTemperatureMQTT.py
@@ -1,6 +1,6 @@
 # humidityTemperatureMQTT.py (c) Jonathan Haddock, @joncojonathan, 2020
 
-# Adapted from https://pimylifeup.com/raspberry-pi-humidity-sensor-dht22/ 
+# Adapted from https://pimylifeup.com/raspberry-pi-humidity-sensor-dht22/
 # also using guidance from https://appcodelabs.com/introduction-to-iot-build-an-mqtt-server-using-raspberry-pi
 # and http://www.steves-internet-guide.com/into-mqtt-python-client/
 # Many thanks!
@@ -21,8 +21,8 @@ PIN_ONE = 4
 PIN_TWO = 17
 
 # MQTT details
-MQTT_BROKER="127.0.0.1"
-MQTT_PORT=1883
+MQTT_BROKER = "127.0.0.1"
+MQTT_PORT = 1883
 
 # Output file name
 LOGFILE = "/home/pi/sensors.csv"
@@ -31,17 +31,20 @@ LOGFILE = "/home/pi/sensors.csv"
 # Only edit below if you know what you're doing!
 ##########################################################
 
-def on_publish(client,userdata,result):             #create function for callback
+
+def on_publish(client, userdata, result):  # create function for callback
     print("data published \n")
     pass
-client1= paho.Client("control1")                    #create client object
-client1.on_publish = on_publish                     #assign function to callback
-client1.connect(MQTT_BROKER,MQTT_PORT)              #establish connection
+
+
+client1 = paho.Client("control1")  # create client object
+client1.on_publish = on_publish  # assign function to callback
+client1.connect(MQTT_BROKER, MQTT_PORT)  # establish connection
 
 try:
     f = open(LOGFILE, 'a+')
     if os.stat(LOGFILE).st_size == 0:
-            f.write('Date,Time,Sensor,Temperature,Humidity\r\n')
+        f.write('Date,Time,Sensor,Temperature,Humidity\r\n')
 except:
     pass
 
@@ -50,25 +53,32 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel_list, GPIO.IN)
 GPIO.add_event_detect(channel_list, GPIO.BOTH)
 
-cupboardHumidity, cupboardTemperature = Adafruit_DHT.read_retry(DHT_SENSOR, PIN_ONE)
+cupboardHumidity, cupboardTemperature = Adafruit_DHT.read_retry(
+    DHT_SENSOR, PIN_ONE)
 
 floorHumidity, floorTemperature = Adafruit_DHT.read_retry(DHT_SENSOR, PIN_TWO)
 
-    
+
 if cupboardHumidity is not None and cupboardTemperature is not None:
-    ret= client1.publish("kitchen/cupboard/temperature","{0:0.1f}".format(cupboardTemperature))
-    ret= client1.publish("kitchen/cupboard/humidity","{0:0.1f}".format(cupboardHumidity))
-    f.write('{0},{1},Cupboard,{2:0.1f}*C,{3:0.1f}%\r\n'.format(time.strftime('%y-%m-%d'), time.strftime('%H:%M'), cupboardTemperature, cupboardHumidity))
+    ret = client1.publish("kitchen/cupboard/temperature",
+                          "{0:0.1f}".format(cupboardTemperature))
+    ret = client1.publish("kitchen/cupboard/humidity",
+                          "{0:0.1f}".format(cupboardHumidity))
+    f.write('{0},{1},Cupboard,{2:0.1f}*C,{3:0.1f}%\r\n'.format(time.strftime('%y-%m-%d'),
+            time.strftime('%H:%M'), cupboardTemperature, cupboardHumidity))
 else:
-    ret= client1.publish("kitchen/cupboard/temperature","FAILED")
+    ret = client1.publish("kitchen/cupboard/temperature", "FAILED")
     print("Failed to retrieve data from cupboard sensor")
 
 if floorHumidity is not None and floorTemperature is not None:
-    ret= client1.publish("kitchen/floor/temperature","{0:0.1f}".format(floorTemperature))
-    ret= client1.publish("kitchen/floor/humidity","{0:0.1f}".format(floorHumidity))
-    f.write('{0},{1},Floor,{2:0.1f}*C,{3:0.1f}%\r\n'.format(time.strftime('%y-%m-%d'), time.strftime('%H:%M'), floorTemperature, floorHumidity))
+    ret = client1.publish("kitchen/floor/temperature",
+                          "{0:0.1f}".format(floorTemperature))
+    ret = client1.publish("kitchen/floor/humidity",
+                          "{0:0.1f}".format(floorHumidity))
+    f.write('{0},{1},Floor,{2:0.1f}*C,{3:0.1f}%\r\n'.format(time.strftime('%y-%m-%d'),
+            time.strftime('%H:%M'), floorTemperature, floorHumidity))
 else:
-    ret= client1.publish("kitchen/floor/temperature","FAILED")
+    ret = client1.publish("kitchen/floor/temperature", "FAILED")
     print("Failed to retrieve data from floor sensor")
 
 # if washingMachineHumidity is not None and washingMachineTemperature is not None:
@@ -79,7 +89,10 @@ else:
 #     ret= client1.publish("kitchen/washing-machine/temperature","FAILED")
 #     print("Failed to retrieve data from washing-machine sensor")
 
+
 def on_disconnect(client, userdata, rc):
-   print("client disconnected ok")
+    print("client disconnected ok")
+
+
 client1.on_disconnect = on_disconnect
 client1.disconnect()
